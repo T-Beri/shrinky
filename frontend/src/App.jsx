b318fc385b1468e5843aa8bae42ea519490cc88d
Tithaye Beri <tithaye.beri@gmail.com>
2025-10-03 19:47:14 +0530

    frontend set up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App
+    <button className='btn btn-soft btn-info'>Cool</button>
+    </div>
+    
+  )
+}
+
+export default App
